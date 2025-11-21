b98a2d58e98955480f83b8d10c67f6b85f8c1a97
Jan200101 <sentrycraft123@gmail.com>
2025-11-21 19:59:03 +0100

    split zig and c compile tests, add link test

--- build.zig
+++ build.zig
@@ -7,6 +7,12 @@ pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    test_compiles_zig(b, target, optimize);
+    test_compiles_c(b, target, optimize);
+    test_linking(b, target, optimize);
+}
+
+fn test_compiles_zig(b: *Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
     const zig_sources = [_][]const u8{
         "src/good.zig",
         "src/bad.zig",
@@ -14,7 +20,7 @@ pub fn build(b: *Build) void {
 
     for (zig_sources) |source| {
         const lib = b.addLibrary(.{
-            .name = "try_make zig",
+            .name = "test_compiles zig",
             .root_module = b.createModule(.{
                 .root_source_file = b.path(source),
                 .target = target,
@@ -26,12 +32,14 @@ pub fn build(b: *Build) void {
         const did_make = make(b, lib);
 
         if (did_make) {
-            std.debug.print("{s}: success\n", .{source});
+            std.debug.print("test_compiles_zig {s}: success\n", .{source});
         } else {
-            std.debug.print("{s} failure\n", .{source});
+            std.debug.print("test_compiles_zig {s}: failure\n", .{source});
         }
     }
+}
 
+fn test_compiles_c(b: *Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
     const c_sources = [_][]const u8{
         "src/good.c",
         "src/bad.c",
@@ -39,7 +47,7 @@ pub fn build(b: *Build) void {
 
     for (c_sources) |source| {
         const lib = b.addLibrary(.{
-            .name = "try_make c",
+            .name = "test_compiles_c",
             .root_module = b.createModule(.{
                 .target = target,
                 .optimize = optimize,
@@ -56,9 +64,40 @@ pub fn build(b: *Build) void {
         const did_make = make(b, lib);
 
         if (did_make) {
-            std.debug.print("{s}: success\n", .{source});
+            std.debug.print("test_compiles_c {s}: success\n", .{source});
         } else {
-            std.debug.print("{s}: failure\n", .{source});
+            std.debug.print("test_compiles_c {s}: failure\n", .{source});
+        }
+    }
+}
+
+fn test_linking(b: *Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
+    const test_syslibs = [_][]const u8{
+        "libcurl",
+        "sdl3",
+        "invalid",
+    };
+
+    for (test_syslibs) |syslib| {
+        const lib = b.addLibrary(.{
+            .name = "try_make zig",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/good.zig"),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{},
+            }),
+        });
+        lib.linkSystemLibrary2(syslib, .{
+            .needed = true,
+        });
+
+        const did_make = make(b, lib);
+
+        if (did_make) {
+            std.debug.print("test_linking {s}: success\n", .{syslib});
+        } else {
+            std.debug.print("test_linking {s}: failure\n", .{syslib});
         }
     }
 }
